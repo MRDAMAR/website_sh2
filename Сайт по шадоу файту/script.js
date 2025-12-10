c774ecd5598b02c722db7ce7e3cc081aa7358549
MRDAMAR <mrdamar60@gmail.com>
2025-12-10 18:45:29 +0200

    1.3 code.js

--- Сайт по шадоу файту/script.js
+++ Сайт по шадоу файту/script.js
@@ -14,3 +14,22 @@ console.log("background-color <body>:", bodyBg);
 const h1 = document.querySelector("h1");
 console.log("font-size <h1>:", getComputedStyle(h1).fontSize);
 console.log("font-size <h1>:", getComputedStyle(h1).fontSize);
+
+// Функція для зміни кольору
+function changeBodyColor() {
+    // Отримуємо поточний колір фону
+    const currentBgColor = getComputedStyle(document.body).backgroundColor;
+
+    // Перевіряємо, чи колір фонового кольору вже червоний
+    if (currentBgColor === "rgb(255, 0, 0)") {
+        // Якщо фон вже червоний, змінюємо його на початковий 
+        document.body.style.backgroundColor = "rgba(255, 255, 255, 1)";
+    } else {
+        // Якщо фон не червоний, змінюємо його на червоний
+        document.body.style.backgroundColor = "red";
+    }
+}
+
+// Кнопка
+const btn = document.getElementById("startBtn");
+btn.addEventListener("click", changeBodyColor);
